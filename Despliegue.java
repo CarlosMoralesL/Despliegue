b696884db5610e15540b7ef4df91d74723b62c0e
CarlosMoralesL <48924874+CarlosMoralesL@users.noreply.github.com>
2019-03-29 12:15:51 +0100

    Despliegue.java

---
+++ Despliegue.java
@@ -0,0 +1,8 @@
+
+public class Despliegue {
+	//Autor Carlos Morales Lucas
+	public static void main(String[] args) {
+		System.out.println("Este es el proyecto de Despliegue de Carlos Morales Lucas");
+	}
+
+}
