6481eedf73c6a5fe5e8e12cc3e6d92cfc75296c8
CarlosMoralesL <carlos.morales.lucas@outlook.com>
2019-04-01 09:57:38 +0200

    Update Prueba.java
    Lo subimos desde github desktop

--- P1/src/Prueba.java
+++ P1/src/Prueba.java
@@ -4,6 +4,6 @@ public class Prueba {
 	public static void main(String[] args) {
 		//Este será el archivo que descargaremos desde Eclipse!
 		//Hemos editado el archivo y procederemos a subir el archivo al repositorio
-
+		//Editamos desde github desktop
 	}
 }
